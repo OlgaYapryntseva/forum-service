554dabb8a669397342b9698bd3955adc25286907
cubic <cubic@host.docker.internal>
2023-09-21 17:55:45 +0300

    First Commit
    Our commit

--- src/main/java/telran/java48/post/dto/PostDto.java
+++ src/main/java/telran/java48/post/dto/PostDto.java
@@ -3,7 +3,6 @@ package telran.java48.post.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
